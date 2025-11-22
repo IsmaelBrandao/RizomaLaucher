61dc11cb4e8774032a6aed5842823b8bc44ca9c9
Ismael <ismaelbrandao2003@gmail.com>
2025-11-22 20:27:48 -0300

    Testes

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/IsmaelBrandao/RizomaLaucher/refs/heads/main/meu_distro.json'
+exports.REMOTE_DISTRO_URL = 'https://ismaelbrandao.github.io/RizomaLaucher/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
