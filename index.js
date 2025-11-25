d8e9cf23fa2eb65f5bd5bfd0cf986b482442e420
Ismael <ismaelbrandao2003@gmail.com>
2025-11-25 16:33:28 -0300

    Ajustes visuais no laucher

--- index.js
+++ index.js
@@ -227,10 +227,13 @@ let win
 function createWindow() {
 
     win = new BrowserWindow({
-        width: 980,
-        height: 552,
+        width: 1280,
+        height: 720,
         icon: getPlatformIcon('SealCircle'),
         frame: false,
+        resizable: false, 
+        maximizable: false,
+        fullscreenable: false,
         webPreferences: {
             preload: path.join(__dirname, 'app', 'assets', 'js', 'preloader.js'),
             nodeIntegration: true,
